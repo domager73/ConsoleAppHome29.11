6d71136ae40061f464f204244f8e1f3aba70b695
domager73 <112970685+domager73@users.noreply.github.com>
2022-11-29 18:30:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppHome29.11/Program.cs
@@ -0,0 +1,21 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+int count = 0;
+for (int i = 1; i <= num / 2; i++)
+{
+    for (int i1 = i; i1 <= num / 2; i1++)
+    {
+        for (int i2 = i1; i2 <= num / 2; i2++)
+        {
+            int chislo = num - (i + i1 + i2);
+            if (chislo >= i2)
+            {
+                if (num == i + i1 + i2 + chislo)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+}
+Console.WriteLine(count);
