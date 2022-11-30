38541a83faf1592126dd725c62becd8523507593
domager73 <112970685+domager73@users.noreply.github.com>
2022-11-30 18:42:26 +0300

    число 3

---
+++ ConsoleAppClassworc/ClassWorc.cs
@@ -0,0 +1,19 @@
+﻿int count = 0;
+
+for (int i = 0; i <= 10; i++)
+{
+    for (int j = 0; j <= 10; j++) 
+    {
+        for (int h = 0; h <= 10; h++) 
+        {
+            for (int g = 0; g <= 10; g++) 
+            {
+                if (i + j == 8 && j + h == 8 && g - h == 6 && i + g == 13) 
+                {
+                    count++;
+                }
+            }
+        }
+    }
+}
+Console.WriteLine(count);
---
+++ ConsoleAppNum3/Program.cs
@@ -0,0 +1,17 @@
+﻿int count = 0; // это счетчик
+int num3 = 0; // поиск числа 3
+Console.WriteLine("Ведите число");
+int num = int.Parse(Console.ReadLine());
+while (num > 0) 
+{
+    num3 = num % 10;
+    Console.WriteLine(num3);
+    if (num3 == 3) 
+    {
+        count++;
+
+    }
+    num = num / 10;
+}
+Console.WriteLine($"число 3 встречаеться {count} раз");
+
